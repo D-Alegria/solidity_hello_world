4f1455ee4f9deba0e34f12a23777d1c005e59f39
"Demilade Oladugba" <"demiladeoladugba@gmail.com">
2022-03-10 13:27:55 +0100

    deployed

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -6,6 +6,10 @@ contract HelloWorld {
     
     string public message;
 
+    constructor (string memory _message) {
+        message = _message;
+    }
+
     function setMessage(string memory _message) public {
         message = _message;
     }
